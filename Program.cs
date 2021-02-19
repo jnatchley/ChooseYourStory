5bb8452a67f10ced759606bdb6fb6cdfed267e0d
Jessica Atchley <jatchley@naver.com>
2021-02-18 20:51:06 -0500

    Asking for players name

--- Program.cs
+++ Program.cs
@@ -4,9 +4,15 @@ namespace ChooseYourStory
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            string CharacterName = "";
+
+            Console.WriteLine("Welcome to the Choose Your own Story Game!");
+            Console.WriteLine("First, what is your Character's name?");
+            CharacterName = Console.ReadLine();
+            Console.WriteLine("Your name is " + CharacterName + "? Oh... great name... It's uh, very creative.");
+            Console.Read();
         }
     }
 }
