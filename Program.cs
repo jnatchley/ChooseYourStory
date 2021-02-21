d4851310d1c7c70ead8a3056d168c1e8ead7b272
Jessica Atchley <jatchley@naver.com>
2021-02-21 18:19:35 -0500

    New class and restructure

--- Program.cs
+++ Program.cs
@@ -2,17 +2,30 @@
 
 namespace ChooseYourStory
 {
+    public static class Game
+    {
+        static string CharacterName = "blank";
+        
+        public static void Start()
+        {
+            Console.WriteLine("Welcome to the Choose Your own Story Game!");
+            NameCharacter();
+        }
+        static void NameCharacter()
+        {
+            Console.WriteLine("First, what is your Character's name?");
+            CharacterName = Console.ReadLine();
+
+            Console.WriteLine("Your name is " + CharacterName + "? Oh... great name... It's uh, very creative.");
+        }
+
+    }
     class Program
     {
         static void Main()
         {
-            string CharacterName = "";
-
-            Console.WriteLine("Welcome to the Choose Your own Story Game!");
-            Console.WriteLine("First, what is your Character's name?");
-            CharacterName = Console.ReadLine();
-            Console.WriteLine("Your name is " + CharacterName + "? Oh... great name... It's uh, very creative.");
-            Console.Read();
+            Game.Start();
+            Console.ReadKey();
         }
     }
 }
