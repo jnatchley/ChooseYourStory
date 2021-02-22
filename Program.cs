fd62eb8f3d705ab79e3a8ca3d755304cb5929a10
Jessica Atchley <jatchley@naver.com>
2021-02-22 11:52:22 -0500

    Added options to choose from

--- Program.cs
+++ Program.cs
@@ -8,8 +8,10 @@ namespace ChooseYourStory
         
         public static void Start()
         {
+             /*Fix this line later, add more info about the story and make it interesting*/
             Console.WriteLine("Welcome to the Choose Your own Story Game!");
             NameCharacter();
+            Options();
         }
         static void NameCharacter()
         {
@@ -17,6 +19,35 @@ namespace ChooseYourStory
             CharacterName = Console.ReadLine();
 
             Console.WriteLine("Your name is " + CharacterName + "? Oh... great name... It's uh, very creative.");
+        }
+        static void Options()
+        {
+            string input = "";
+            /*Fix this line later, make it interesting*/
+            Console.WriteLine("You gotta go to either A, B, C, D. Choose one.");
+            input = Console.ReadLine();
+            input = input.ToUpper();
+            if (input == "A")
+            {
+                Console.WriteLine ("Blah Blah Good ending");
+            }
+            else if (input == "B")
+            {
+                Console.WriteLine ("Blah blah Good Ending 2");
+            }
+            else if (input == "C")
+            {
+                Console.WriteLine ("Weird Ending");
+            }
+            else if (input == "D")
+            {
+                Console.WriteLine ("Bad Ending");
+            }
+            else
+            {
+                Console.WriteLine ("What are you doing ending");
+            }
+
         }
 
     }
